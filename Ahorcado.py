55e812da40c7de8383415409dba6cbea5db11568
AdryanOvie2 <adryanovie2@gmail.com>
2023-12-03 13:42:46 +0100

    finished ahorcado

--- Ahorcado.py
+++ Ahorcado.py
@@ -2,6 +2,7 @@ import random
 import string
 
 from palabras import palabras
+from diagrama import vidas_diccionario_visual
 
 
 def obtener_palabra_valida(palabras):
@@ -25,3 +26,35 @@ def ahorcado():
     abc = set(string.ascii_uppercase)
 
     vidas = 7
+
+    while len(letras_por_adivinar) > 0 and vidas > 0:
+        print(f"Te quedan {vidas} y has usado estas letras: {' '.join(letras_adivinadas)}")
+
+        palabra_lista = [letra if letra in letras_adivinadas else '-' for letra in palabra]
+        print(vidas_diccionario_visual[vidas])
+        print(f"Palabra: {' '.join(palabra_lista)}")
+
+        letra_usuario = input("Escoge una letra: ").upper()
+
+        if letra_usuario in abc - letras_adivinadas:
+            letras_adivinadas.add(letra_usuario)
+
+            if letra_usuario in letras_por_adivinar:
+                letras_por_adivinar.remove(letra_usuario)
+                print('')
+            else:
+                vidas -= 1
+                print(f"\nTu letra, {letra_usuario} no está en la palabra.")
+        elif letra_usuario in letra_usuario:
+            print("\nYa escogiste esa letra. Por favor escoge una letra nueva.")
+        else:
+            print("\nEsta letra no es válida.")
+
+    if vidas == 0:
+        print(vidas_diccionario_visual[vidas])
+        print(f"\n¡Ahorcado! Perdiste. Lo siento mucho. La palabra era {palabra}")
+    else:
+        print(f"¡Excelente! ¡Adivinaste la palabra {palabra}!")
+
+
+ahorcado()
